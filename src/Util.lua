519df0f7f5a8927f2e771beb72f512b1f3515680
Matt <mbgeerling@gmail.com>
2021-02-17 17:47:12 -0800

    first upload

---
+++ src/Util.lua
@@ -0,0 +1,162 @@
+--[[
+    GD50
+    Breakout Remake
+
+    -- StartState Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Helper functions for writing games.
+]]
+
+--[[ 
+    MBG - some notes about positions 
+    key: y = 48, x = 32*5 - dimensions are 32x16 (x,y)
+    balls: y = 48, x = 32*3 - dimensions are 8x8 - 7 balls (across two rows)
+    powerups: x = 0, y = 12*16; dimensions are 16x16 (10 including key)
+    key powerup: x = 32*5 - 16, y = 12*16
+
+]]
+
+--[[
+    Given an "atlas" (a texture with multiple sprites), as well as a
+    width and a height for the tiles therein, split the texture into
+    all of the quads by simply dividing it evenly.
+]]
+function GenerateQuads(atlas, tilewidth, tileheight)
+    local sheetWidth = atlas:getWidth() / tilewidth
+    local sheetHeight = atlas:getHeight() / tileheight
+
+    local sheetCounter = 1
+    local spritesheet = {}
+
+    for y = 0, sheetHeight - 1 do
+        for x = 0, sheetWidth - 1 do
+            spritesheet[sheetCounter] =
+                love.graphics.newQuad(x * tilewidth, y * tileheight, tilewidth,
+                tileheight, atlas:getDimensions())
+            sheetCounter = sheetCounter + 1
+        end
+    end
+
+    return spritesheet
+end
+
+--[[
+    Utility function for slicing tables, a la Python.
+
+    https://stackoverflow.com/questions/24821045/does-lua-have-something-like-pythons-slice
+]]
+function table.slice(tbl, first, last, step)
+    local sliced = {}
+  
+    for i = first or 1, last or #tbl, step or 1 do
+      sliced[#sliced+1] = tbl[i]
+    end
+  
+    return sliced
+end
+
+--[[
+    This function is specifically made to piece out the bricks from the
+    sprite sheet. Since the sprite sheet has non-uniform sprites within,
+    we have to return a subset of GenerateQuads.
+]]
+function GenerateQuadsBricks(atlas)
+    return table.slice(GenerateQuads(atlas, 32, 16), 1, 21)
+end
+
+--[[
+    This function is specifically made to piece out the paddles from the
+    sprite sheet. For this, we have to piece out the paddles a little more
+    manually, since they are all different sizes.
+]]
+function GenerateQuadsPaddles(atlas)
+    local x = 0
+    local y = 64
+
+    local counter = 1
+    local quads = {}
+
+    for i = 0, 3 do
+        -- smallest
+        quads[counter] = love.graphics.newQuad(x, y, 32, 16,
+            atlas:getDimensions())
+        counter = counter + 1
+        -- medium
+        quads[counter] = love.graphics.newQuad(x + 32, y, 64, 16,
+            atlas:getDimensions())
+        counter = counter + 1
+        -- large
+        quads[counter] = love.graphics.newQuad(x + 96, y, 96, 16,
+            atlas:getDimensions())
+        counter = counter + 1
+        -- huge
+        quads[counter] = love.graphics.newQuad(x, y + 16, 128, 16,
+            atlas:getDimensions())
+        counter = counter + 1
+
+        -- prepare X and Y for the next set of paddles
+        x = 0
+        y = y + 32
+    end
+
+    return quads
+end
+
+--[[
+    This function is specifically made to piece out the balls from the
+    sprite sheet. For this, we have to piece out the balls a little more
+    manually, since they are in an awkward part of the sheet and small.
+]]
+function GenerateQuadsBalls(atlas)
+    local x = 96
+    local y = 48
+
+    local counter = 1
+    local quads = {}
+
+    for i = 0, 3 do
+        quads[counter] = love.graphics.newQuad(x, y, 8, 8, atlas:getDimensions())
+        x = x + 8
+        counter = counter + 1
+    end
+
+    x = 96
+    y = 56
+
+    for i = 0, 2 do
+        quads[counter] = love.graphics.newQuad(x, y, 8, 8, atlas:getDimensions())
+        x = x + 8
+        counter = counter + 1
+    end
+
+    return quads
+end
+
+function GenerateQuadsPowerups(atlas)
+    --    powerups: x = 0, y = 12*16; dimensions are 16x16 (10 including key)
+    local x = 0
+    local y = 12*16 
+
+    local counter = 1 
+    local quads = {}
+
+    for i = 0, 9 do --make sure this gets the last one 
+        quads[counter] = love.graphics.newQuad(x, y, 16, 16, atlas:getDimensions())
+        x = x + 8
+        counter = counter + 1
+    end
+    return quads
+end 
+
+function GenerateKeyBrick(atlas)
+    --key: y = 48, x = 32*5 - dimensions are 32x16 (x,y)
+    local x = 32*5
+    local y = 48
+
+    quads = love.graphics.newQuad(x,y,32,16,atlas:getDimensions())
+    return quads
+
+end 
